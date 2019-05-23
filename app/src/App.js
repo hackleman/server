8c4cdaf50a7c0804f9d7bcf36ea9e1ceb55cf597
hackleman <hackleman.jason@gmail.com>
2019-05-23 14:30:47 -0500

    redux

--- app/src/App.js
+++ app/src/App.js
@@ -2,15 +2,21 @@ import React, {Component} from 'react';
 import AppNavbar from './components/appnavbar';
 import ShoppingList from './components/shoppinglist';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar/>
-      <ShoppingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar/>
+        <ShoppingList/>
+      </div>
+    </Provider>
+
   );
 }
 
